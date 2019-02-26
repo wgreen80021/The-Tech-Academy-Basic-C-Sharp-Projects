820a7f1963fee004b5ad465d56fbddc3b739eda1
WA <45290206+wgreen80021@users.noreply.github.com>
2019-02-26 10:33:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you !");
+        Console.ReadLine();
+    }
+}
+

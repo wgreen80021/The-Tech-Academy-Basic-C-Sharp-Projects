8b68ed85ba09b6f0f669ba7ef69a4b393958f68f
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-02 06:03:51 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace strings1
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        { 
+            string strSent1 = "This is part one ";
+            string strSent2 = " This is part2 ";
+            string strSent3 = " This is Part 3 \n";
+            string sbL1 = "This is line one of the paragraph I am going to string. \n";
+            string sbL2 = "This is line two of the paragraph I am going to string.\n";
+            string sbL3 = "This is line three of the paragraph I am going to string.\n";
+            string sbL4 = "This is line four of the paragraph I am going to string. \n";
+            string sbL5 = "This is line five of the paragraph I am going to string. \n";
+            string sbL6 = "This is line six of the paragraph I am going to string. \n";
+
+
+
+            string concSent = strSent1.ToUpper() + strSent2 + strSent3;
+           
+
+            StringBuilder sb = new StringBuilder(concSent);
+
+           
+            sb.Append(sbL1);
+            sb.Append(sbL2);
+            sb.Append(sbL3);
+            sb.Append(sbL4);
+            sb.Append(sbL5);
+            sb.Append(sbL6);
+
+            
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
     }
 }
-
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("strings1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("strings1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a51f81c-4ce2-4edd-aba5-7cc8ddfc10de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

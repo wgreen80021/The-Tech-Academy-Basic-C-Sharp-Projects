4b86a009a8bcd5f159409876ff0158003cc481c6
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-01 20:16:31 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Switch
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+
+            int x = 0;
+            int i = 0;
+            while (x < 5)
+            {
+                i = 0;
+                x = x + 1;
+                do
+                {
+                    Console.WriteLine("X = " + x + " I = " + i);
+                    i = i + 1;
+
+
+                } while (i < 5);
+                
+
+
+            }
+            Console.ReadLine();
+        }
     }
 }
-

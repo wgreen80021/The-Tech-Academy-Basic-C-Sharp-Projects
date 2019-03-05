c6ff05080a92c091ea31259f3209b7d24409144a
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-05 11:00:54 -0700

    Add files via upload

---
+++ MathDrill.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+     class MathDrill
+    {
+
+         
+        
+
+          public  int AddValues(int x, int y)
+            {
+                return x + y;
+            }
+
+          public  int SubrtractValues(int x, int y)
+            {
+                return x - y;
+            }
+
+            public int MultiplyValues(int x, int y)
+            {
+                return x * y;
+            }
+
+             
+        }
+         
+    }
+
+    
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace ClassDrill
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number that will have 5 added to it , 5 subtracted from it, and multiplied by 5 :");
+            string strInNum = Console.ReadLine();
+            int b = Convert.ToInt32(strInNum);
+            MathDrill a = new MathDrill();
+                int x = a.AddValues (5, b);
+                int y = a.SubrtractValues(5, b);
+                int z = a.MultiplyValues (5, b);
+            
+            Console.WriteLine( "5 Plus " + strInNum + " = "  + x);
+                Console.WriteLine("5 Minus " + strInNum + " = "  + y);
+            Console.WriteLine( "5 Multiplied by " + strInNum +  "  = " + z);
+            
+            Console.ReadLine();
+
+        }
     }
 }
-

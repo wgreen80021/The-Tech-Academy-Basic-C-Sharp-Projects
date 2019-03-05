cf07db202bd4cbe3dfc563b35f89c90d04fca20b
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-05 12:55:42 -0700

    Add files via upload

---
+++ DrillClass2/ClassAdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillClass2
+{
+    class ClassAdd
+    {
+        public void AddValues(int x, int y)
+        {
+            for (int i = 0; i < y; i++)
+
+                x = x + y;
+             
+
+            Console.WriteLine(y + " is the second number");
+            Console.ReadLine();
+
+        }
+
+            
+    }
+}
---
+++ DrillClass2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillClass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassAdd a = new ClassAdd();
+
+             a.AddValues(x:5, y:2);
+
+            
+        }
+    }
+}

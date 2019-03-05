2cc0bbdea746dd10bb6cfa4264f74ccc2ddc0ab5
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-05 14:01:38 -0700

    Add files via upload

---
+++ ClassDrill4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill4
+{
+    class ClassDrill4
+    {
+
+        public int AddValues(int x, int y = 1)
+        {
+            return x + y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace ClassDrill4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number that will be added to a second number :");
+            string strInNum1 = Console.ReadLine();
+
+            Console.WriteLine("You may enter a second number or leave blank to use the default :");
+            
+            string strInNum2 = Console.ReadLine();
+
+            ClassDrill4 a = new ClassDrill4();
+
+            switch  (strInNum2)
+            {
+                case "":
+                    int x = a.AddValues(Convert.ToInt32(strInNum1));
+                    Console.WriteLine("Your answer is : " + x + " using the default value of 1");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    int z = a.AddValues(Convert.ToInt32(strInNum1), Convert.ToInt32(strInNum2));
+                    Console.WriteLine("Your answer is : " + z );
+                    Console.ReadLine();
+
+                    break;
+
+                    
+            }
+
+        }
     }
 }
-

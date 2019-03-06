9e3bb414663a6fc4557059f5245a4b856e28a127
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-05 20:22:27 -0700

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Employee:Person
+    {
+        public int id { get; set; }
+
+
+    }
+}

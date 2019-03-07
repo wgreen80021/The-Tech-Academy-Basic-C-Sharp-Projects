e3a3103d1959ade2e2ff23ef017fa771bb0d3d63
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-07 15:38:56 -0700

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    
+
+    public class Employee:Person, IQuittable
+    {
+        public int id { get; set; }
+
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("I Quit");
+            //throw new NotImplementedException();
+        }
+    }
+    
+}

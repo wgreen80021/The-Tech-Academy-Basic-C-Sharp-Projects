1ee5c98679bd8f5feec9bb468e2513cce34d5908
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-15 11:11:31 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace DrillIO2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time Right Now is : {0}",  DateTime.Now);
+            Console.WriteLine("Please enter number of hours to add : ");
+            double dateOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The adjusted time is : {0}",  DateTime.Now.AddHours(dateOne));
+            Console.ReadLine();
+            
+             
+
+        }
     }
 }
-

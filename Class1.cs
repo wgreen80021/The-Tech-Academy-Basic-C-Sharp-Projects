ddbb8a6f782b00326cd231d336a98ad9818ffebc
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-16 06:47:20 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillsVar1
+{
+    class Class1
+    {
+
+        public Class1(string Fname, string Lname, string Address)
+        {    
+            e_Fname = Fname;
+            e_Lname = Lname;
+            e_Address = Address;
+        }
+
+         
+       
+        public Class1(string Fname, string Lname) : this(Fname, Lname, "123 Main Street")
+        {
+        }
+
+        public string e_Fname, e_Lname, e_Address;
+        
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace DrillsVar1
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+
+
+        static void Main(string[] args)
+        {
+            var x = "Wendy";
+            const string cAddr = "777 Lucky Lane";
+
+            Class1 myClass = new Class1(x, "Smith", cAddr);
+
+            Console.WriteLine("Example assigning all three values : {0} {1} {2}" , myClass.e_Fname, myClass.e_Lname, myClass.e_Address) ;
+            Console.ReadLine();
+
+            Class1 myClass2 = new Class1(x, "Smith"); 
+
+            Console.WriteLine("Example assigning only two values : {0} {1} {2}", myClass2.e_Fname, myClass2.e_Lname, myClass2.e_Address);
+            Console.ReadLine();
+             
+        }
+        
     }
 }
-

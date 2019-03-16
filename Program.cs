ff8ed627e0b37c78241c8e57fca4f1b2f97ace2f
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-16 09:30:43 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Except1
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're \"accounting\" on you !");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            bool ValidAge = false;
+            int Age = 0;
+            DateTime dateNow = DateTime.Today;
+            
+            try
+            {
+                Console.WriteLine("Please enter your age:  ");
+                 
+                
+
+                
+                ValidAge = int.TryParse(Console.ReadLine(), out Age);
+                if (ValidAge && (Age > 0))
+                {
+                    DateTime dateBorn = dateNow.Date.AddYears(Age * -1);
+                    Console.WriteLine("Your Birthdate is {0}", dateBorn);
+                    Console.ReadLine();
+                    return;
+                }
+                {
+                    if (Age <= 0 && ValidAge)
+                    {
+
+                        throw new FormatException();
+                    }
+                }  
+                { 
+                    
+                        throw new Exception();
+                }
+          
+                
+                 
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Entry cannot be 0 or less ");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Entry made .....");
+                Console.ReadLine();
+                
+                
+            }
+        }
+
     }
+
+
+
 }
+    
 
